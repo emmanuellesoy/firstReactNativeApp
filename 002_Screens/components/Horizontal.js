e5a46d49e4faddda923e07d2d8834e38bc223ed8
sh4nn0nb1t <emmanuelle.laguna@televisatim.com>
2019-05-14 14:05:48 -0500

    Change Color Views

--- 002_Screens/components/Horizontal.js
+++ 002_Screens/components/Horizontal.js
@@ -8,19 +8,19 @@ import {
 } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        
-    },
     outer: {
-        backgroundColor: "#007bb8",
+        backgroundColor: "#0E1C3F",
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
         width: Dimensions.get("window").width,
         height: Dimensions.get("window").height,
     },
-    outerRed: {
-        backgroundColor: "#e01931",
+    red: {
+        backgroundColor: "#EE0033",
+    },
+    green: {
+        backgroundColor: "#02A574",
     },
     innerText: {
         color: "#eeefef",
@@ -34,16 +34,10 @@ const Horizontal = () => (
         <View style={styles.outer}>
             <Text style={styles.innerText}>Welcome to sh4app</Text>
         </View>
-        <View style={[styles.outer, styles.outerRed]}>
+        <View style={[styles.outer, styles.red]}>
             <Text style={styles.innerText}>Welcome to sh4app</Text>
         </View>
-        <View style={styles.outer}>
-            <Text style={styles.innerText}>Best quality videos</Text>
-        </View>
-        <View style={[styles.outer, styles.outerRed]}>
-            <Text style={styles.innerText}>Aviable on iOS and Android</Text>
-        </View>
-        <View style={styles.outer}>
+        <View style={[styles.outer, styles.green]}>
             <Text style={styles.innerText}>Best quality videos</Text>
         </View>
     </ScrollView>
