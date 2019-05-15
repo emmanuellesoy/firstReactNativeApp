a9c333ed2cca4eedad5813da1f3e52accdf17520
sh4nn0nb1t <emmanuelle.laguna@televisatim.com>
2019-05-15 02:20:03 -0500

    Adding a header

--- 004_Shoping/App.js
+++ 004_Shoping/App.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import Login from "./components/Login";
+import {
+    SafeAreaView,
+    StyleSheet,
+    View
+} from "react-native";
+import Header from "./components/Header";
 
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#6BEAC4",
-        alignItems: "center",
-        justifyContent: "center",
     },
 });
 
 const App = () => (
-    <View style={styles.container}>
-        <Login />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <Header />
+    </SafeAreaView>
 );
 
 export default App;
---
+++ 004_Shoping/components/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+    Image,
+    StyleSheet,
+    Text,
+    View
+} from "react-native";
+
+const LOGO = require("../assets/icon.png");
+
+const styles = StyleSheet.create({
+    header: {
+        height: 80,
+        backgroundColor: "#f5f5f5",
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 20,
+        borderBottomWidth: 4,
+        borderColor: "#464c53"
+    },
+    logo: {
+        width: 32,
+        height: 32,
+    },
+    text: {
+        marginLeft: 10,
+        color: "#464c53",
+        fontWeight: "bold",
+        fontSize: 16,
+        fontStyle: "italic",
+    }
+});
+
+const Header = () => (
+    <View style={styles.header}>
+        <Image
+            source={LOGO}
+            style={styles.logo}
+        />
+        <Text style={styles.text}>
+            sh4nn0nb1t Shopping Cart
+        </Text>
+    </View>
+);
+
+export default Header;
